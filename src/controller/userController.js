9823a73a7e4cfb7c14cc36147de253407b46f248
KAUAN LOPES PEREIRA <25132925@CFP127ALUNO.LOCAL>
2025-08-27 11:24:18 -0300

    Criação da função para controle de lista de usuarios

---
+++ src/controller/userController.js
@@ -0,0 +1,30 @@
+'use strict'
+
+/********************************************************************************************
+* Objetivo: Arquivo responsavel pelo controle das requisições executadas pelo front
+* Autor: Kauan Lopes Pereira
+* Data: 27/08/2025
+* Versão: 1.0
+********************************************************************************************/
+
+/* Comentário em bloco */
+// Comentário em linha
+
+/********************************************************************************************
+********************************* COMANDOS UTILIZADOS ***************************************
+
+
+********************************************************************************************/
+
+// Importação das funções presentes no userModel
+const userModel = require('../model/userModel')
+
+// Controlador para listar todos os usuarios
+const getAllUsers = (req, res) => {
+    const users = userModel.findAll()
+    res.status(200).json9(users)
+}
+
+module.exports = {
+    getAllUsers
+}
