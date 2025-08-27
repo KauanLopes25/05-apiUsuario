b9fa4c52ec8773800122dd87069cee3e5bb27710
KAUAN LOPES PEREIRA <25132925@CFP127ALUNO.LOCAL>
2025-08-27 10:17:28 -0300

    Criação de um array de objetos

--- src/model/userModel.js
+++ src/model/userModel.js
@@ -1,1 +1,27 @@
 'use strict'
+
+/********************************************************************************************
+* Objetivo: Arquivo responsavel pela comunicação com o banco de dados
+* Autor: Kauan Lopes Pereira
+* Data: 27/08/2025
+* Versão: 1.0
+********************************************************************************************/
+
+/* Comentário em bloco */
+// Comentário em linha
+
+/********************************************************************************************
+********************************* COMANDOS UTILIZADOS ***************************************
+
+
+******************************** BIBLIOTECAS UTILIZADAS *************************************
+
+
+********************************************************************************************/
+
+// Simulação de um banco de dados em memória
+let users = [
+    {id: 1, name: 'Alice', email: 'alice@gmail.com'},
+    {id: 1, name: 'Maximus', email: 'maximus@gmail.com'},
+    {id: 1, name: 'Ragnar', email: 'ragnar@gmail.com'}
+]
